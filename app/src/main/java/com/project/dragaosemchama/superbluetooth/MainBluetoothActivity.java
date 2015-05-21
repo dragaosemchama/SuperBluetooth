7c2daac894e93445b29369d9f59307e94eb6a7c8
dave <oblivion.davecave@gmail.com>
2015-05-21 16:35:32 -0300

    Initial commit.

---
+++ app/src/main/java/com/project/dragaosemchama/superbluetooth/MainBluetoothActivity.java
@@ -0,0 +1,147 @@
+package com.project.dragaosemchama.superbluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainBluetoothActivity extends ActionBarActivity {
+
+    public static int ENABLE_BLUETOOTH = 1;
+    public static int SELECT_PAIRED_DEVICE = 2;
+    public static int SELECT_DISCOVERED_DEVICE = 3;
+
+    static TextView statusMessage;
+    static TextView textSpace;
+    ConnectionThread connect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bluetooth);
+
+        statusMessage = (TextView) findViewById(R.id.statusMessage);
+        textSpace = (TextView) findViewById(R.id.textSpace);
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter == null) {
+            statusMessage.setText("Que pena! Hardware Bluetooth não está funcionando :(");
+        } else {
+            statusMessage.setText("Ótimo! Hardware Bluetooth está funcionando :)");
+            if(!btAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH);
+                statusMessage.setText("Solicitando ativação do Bluetooth...");
+            } else {
+                statusMessage.setText("Bluetooth já ativado :)");
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_bluetooth, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == ENABLE_BLUETOOTH) {
+            if(resultCode == RESULT_OK) {
+                statusMessage.setText("Bluetooth ativado :D");
+            }
+            else {
+                statusMessage.setText("Bluetooth não ativado :(");
+            }
+        }
+        else if(requestCode == SELECT_PAIRED_DEVICE || requestCode == SELECT_DISCOVERED_DEVICE) {
+            if(resultCode == RESULT_OK) {
+                statusMessage.setText("Você selecionou " + data.getStringExtra("btDevName") + "\n"
+                                        + data.getStringExtra("btDevAddress"));
+
+                connect = new ConnectionThread(data.getStringExtra("btDevAddress"));
+                connect.start();
+            }
+            else {
+                statusMessage.setText("Nenhum dispositivo selecionado :(");
+            }
+        }
+    }
+
+    public void searchPairedDevices(View view) {
+
+        Intent searchPairedDevicesIntent = new Intent(this, PairedDevices.class);
+        startActivityForResult(searchPairedDevicesIntent, SELECT_PAIRED_DEVICE);
+    }
+
+    public void discoverDevices(View view) {
+
+        Intent searchPairedDevicesIntent = new Intent(this, DiscoveredDevices.class);
+        startActivityForResult(searchPairedDevicesIntent, SELECT_DISCOVERED_DEVICE);
+    }
+
+    public void enableVisibility(View view) {
+
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 30);
+        startActivity(discoverableIntent);
+    }
+
+    public void waitConnection(View view) {
+
+        connect = new ConnectionThread();
+        connect.start();
+    }
+
+    public void sendMessage(View view) {
+
+        EditText messageBox = (EditText) findViewById(R.id.editText_MessageBox);
+        String messageBoxString = messageBox.getText().toString();
+        byte[] data =  messageBoxString.getBytes();
+        connect.write(data);
+    }
+
+    public static Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            Bundle bundle = msg.getData();
+            byte[] data = bundle.getByteArray("data");
+            String dataString= new String(data);
+
+            if(dataString.equals("---N"))
+                statusMessage.setText("Ocorreu um erro durante a conexão D:");
+            else if(dataString.equals("---S"))
+                statusMessage.setText("Conectado :D");
+            else {
+
+                textSpace.setText(new String(data));
+            }
+        }
+    };
+}
